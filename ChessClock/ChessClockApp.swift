fcfae442e51bb5bc2bdd9c1f52f846dc69fc3e30
Keith Staines <keith.staines@workfinder.com>
2021-12-25 09:19:44 +0000

    Initial Commit

---
+++ ChessClock/ChessClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessClockApp.swift
+//  ChessClock
+//
+//  Created by Keith Staines on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ChessClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
